6095621675fba4f734c87b039d10d8723ae4f0e4
Jose Pedro Fuxi Andre <josefuxi2000@gmail.com>
2026-08-12 15:47:02 +0100

    Testing templete's concepts

---
+++ concepts/variadic_tamplates.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+
+template <typename T1>
+int myNumber(T1 ex)
+{
+    std::vector<T1> My_list;
+    My_list.push_back(ex);
+    std::cout << My_list.front() << std::endl;
+    return 0;
+}   
+
+int main(){
+
+    myNumber("Jose Pedro Fuxi André");
+}
