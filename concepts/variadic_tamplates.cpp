e3f28a4256acee2db52180e4d40ef456703babc6
Jose Pedro Fuxi Andre <josefuxi2000@gmail.com>
2026-08-12 22:20:05 +0100

    Refactor variadic_tamplates.cpp to improve code structure and readability

--- concepts/variadic_tamplates.cpp
+++ concepts/variadic_tamplates.cpp
@@ -2,6 +2,13 @@
 #include <vector>
 #include <algorithm>
 
+//simple templetes
+
+// template <typename T>
+// void printl(T t){
+//     std::cout << t << std::endl;
+// }
+
 template <typename T1>
 int myNumber(T1 ex)
 {
@@ -12,8 +19,41 @@ int myNumber(T1 ex)
     return 0;
 }
 
+template <class myType, int src>
+int minValue(myType arr, int size)
+{
+    int base = src;
+
+    for (int i = 0; i < size; i++)
+    {
+        if (arr[i] < base)
+            base = arr[i];
+    }
+    std::cout << "The min value is = " <<  base << std::endl;
+    return base;
+}
+
+
+// Variadic Templates
+void printl(){return ;}
+
+template <typename T, typename... Others>
+void printl(T frst, Others... scd)
+{
+    std::cout << frst <<std:: endl;
+    printl(scd...);
+}
 int main(){
 
+    std::vector<std::string> B {"joandre 42"};
+    for (int i = 0; i < 20; i++)
+    {
+        B.push_back(std::to_string(i));
+    }
     myNumber<std::string>("Jose Pedro Fuxi André");
-    // myNumber(12);
+    myNumber<std::vector<std::string>>(B);
+    int arr[] = {20,1, 4, 6};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    minValue<int[], -100>(arr, size);
+    printl<>("Hello", "World", "Variadic", "Templates", 42, 3.14);
 }
